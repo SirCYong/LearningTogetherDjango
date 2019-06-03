bd13e097cd631d8e4ce094693f859cfe77fd1c97
cy <micheal.caoy@gmail.com>
2019-06-03 21:48:10 +0800

    --led 灯 闪烁

--- myapp/one_led.py
+++ myapp/one_led.py
@@ -4,7 +4,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(21,GPIO.OUT)
 while True:
     GPIO.output(21,GPIO.HIGH)
-    time.sleep(0.2)
+    time.sleep(10)
     GPIO.output(21,GPIO.LOW)
-    time.sleep(0.2)
+    time.sleep(10)
 
