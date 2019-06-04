550604cb0616e5c91d50063ce7b8f95d999eeedd
cy <micheal.caoy@gmail.com>
2019-06-04 23:22:04 +0800

    --led

--- myapp/one_people.py
+++ myapp/one_people.py
@@ -11,18 +11,25 @@ import time
 # 如果上面GPIO口的选定模式指定为主板模式的话，这里就应该指定8号而不是14号。
 # driver.setup(23, driver.IN)
 number = 8
+led_num = 23
+"""
+有人灯就亮 2s 没人 就关灯
+"""
 while True:
     driver.setmode(driver.BCM)
-
+    driver.setup(led_num, driver.OUT)
     driver.setup(number, driver.IN)
 
     if driver.input(number):
-        print("有人")
+        driver.output(led_num, True)
+        continue
     else:
         print("没人")
+        driver.output(led_num, False)
     time.sleep(2)
     driver.cleanup()
 
 # 最后清理GPIO口（不做也可以，建议每次程序结束时清理一下，好习惯）
+
 
 
