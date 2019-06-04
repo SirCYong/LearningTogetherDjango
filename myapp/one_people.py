231b786ba63189523ff2e7e09940a1b5e2c0ebbb
cy <micheal.caoy@gmail.com>
2019-06-04 22:37:53 +0800

    --有人没人

--- myapp/one_people.py
+++ myapp/one_people.py
@@ -5,13 +5,15 @@ import RPi.GPIO as driver
 import time
 
 # 指定GPIO口的选定模式为GPIO引脚编号模式（而非主板编号模式）
-driver.setmode(driver.BCM)
+# driver.setmode(driver.BCM)
 
 # 指定GPIO14（就是LED长针连接的GPIO针脚）的模式为输出模式
 # 如果上面GPIO口的选定模式指定为主板模式的话，这里就应该指定8号而不是14号。
 # driver.setup(23, driver.IN)
 
 while True:
+    driver.setmode(driver.BCM)
+
     driver.setup(23, driver.IN)
 
     if driver.input(23):
